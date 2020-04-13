b7522b901cf998697c78e856445acc53a0b646b0
usanator <uahme002@ucr.edu>
2020-04-13 16:44:48 -0700

    Update part3.sh

--- Lab-2/part3.sh
+++ Lab-2/part3.sh
@@ -3,23 +3,19 @@
 FILE=$1
 LOG=$2
 
-declare -i counter1=0
-declare -i counter2=0
-
 if [ -e "$FILE" ]; then
-	if[ "$counter1" -eq 0 ]; then
+	if[ $(tail -n 1 "$LOG" | grep "lost" | wc -1) -eq 1 ]; then
 		echo "$(date +"%Y-%m-%d %T) - File \"$FILE\" has been found" >> $LOG
-		counter1=$((counter1 + 1))
 	fi
-	if[ "$counter1" -ge 1 ]; then
+	if[ $(tail -n 1 "$LOG" | grep "lost" | wc -1) -eq 0 ]; then
 		echo "found" >> $LOG
 	fi
 else
-	if[ "$counter2" -eq 0 ]; then
+	if[ $(tail -n 1 "$LOG" | grep "found" | wc -1) -eq 1 ]; then
 		echo "$(date +"%Y-%m-%d %T) - File \"$FILE\" has been lost" >> $LOG
 		counter2=$((counter1 + 1))
 	fi
-	if[ "$counter2" -ge 1 ]; then
+	if[ $(tail -n 1 "$LOG" | grep "found" | wc -1) -eq 0 ]; then
 		echo "lost" >> $LOG
 		
 	fi
