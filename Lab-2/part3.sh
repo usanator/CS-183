5be84883cafcee9739551513ed580c9e8a7e715b
usanator <uahme002@ucr.edu>
2020-04-12 17:37:33 -0700

    Update part3.sh

--- Lab-2/part3.sh
+++ Lab-2/part3.sh
@@ -3,18 +3,23 @@
 FILE=$1
 LOG=$2
 
+declare -i counter1=0
+declare -i counter2=0
+
 if [ -e "$FILE" ]; then
-	if[];
+	if[ "$counter1" -eq 0 ];
 		echo "$(date +"%Y-%m-%d %T) - File \"$FILE\" has been found" >> $LOG
+		counter1=$((counter1 + 1))
 	fi
-	if[]; then
+	if[ "$counter1" -ge 1 ]; then
 		echo "found" >> $LOG
 	fi
 else
-	if[]; then
+	if[ "$counter2" -eq 0 ]; then
 		echo "$(date +"%Y-%m-%d %T) - File \"$FILE\" has been lost" >> $LOG
+		counter2=$((counter1 + 1))
 	fi
-	if[]; then
+	if[ "$counter2" -ge 1 ]; then
 		echo "lost" >> $LOG
 		
 	fi
