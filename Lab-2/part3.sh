68441e33d53e3830d84ae16b0a4221b79c5d0ee2
usanator <uahme002@ucr.edu>
2020-04-14 14:44:14 -0700

    Update part3.sh

--- Lab-2/part3.sh
+++ Lab-2/part3.sh
@@ -7,7 +7,7 @@ if [ -e "$FILE" ]; then
 	if[ $(tail -n 1 "$LOG" | grep "lost" | wc -1) -eq 1 ]; then
 		echo "$(date +"%Y-%m-%d %T) - File \"$FILE\" has been found" >> $LOG
 	fi
-	if[ $(tail -n 1 "$LOG" | grep "lost" | wc -1) -eq 0 ]; then
+	if[ $(cat $LOG | wc -1) -eq 0 ]; then
 		echo "found" >> $LOG
 	fi
 else
@@ -15,7 +15,7 @@ else
 		echo "$(date +"%Y-%m-%d %T) - File \"$FILE\" has been lost" >> $LOG
 		counter2=$((counter1 + 1))
 	fi
-	if[ $(tail -n 1 "$LOG" | grep "found" | wc -1) -eq 0 ]; then
+	if[ $(cat $LOG | wc -1) -eq 0 ]; then
 		echo "lost" >> $LOG
 		
 	fi
